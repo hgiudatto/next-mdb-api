1e6c1ebb7b402cd57fbd81567202bb15b0c8ee6d
hgiudatto <hector.giudatto@gmail.com>
2022-10-07 18:58:50 -0300

    Next Js Crash Course

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,9 @@
-import React from "react";
-import Head from "next/head";
+import Meta from "../components/Meta";
 
 const about = () => {
   return (
     <div>
-      <Head>
-        <title>About</title>
-      </Head>
+      <Meta title="About" />
       <h1>About</h1>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,35 @@
-import Head from "next/head";
+import { server } from "../config";
+import ArticleList from "../components/ArticleList";
 import Image from "next/image";
 import styles from "../styles/Layout.module.css";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name="keywords" content="web development, programming" />
-      </Head>
-
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${server}/api/articles`);
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+/* export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+}; */
